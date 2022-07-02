09b7304ca5d2ae3a85752bf55d6a65fc98dfb3cb
song <songsongsongggg@163.com>
2022-07-02 14:00:23 +0800

    2-2

---
+++ kb/src/main/java/com/song/kb/KbApplication.java
@@ -0,0 +1,13 @@
+package com.song.kb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KbApplication.class, args);
+    }
+
+}
