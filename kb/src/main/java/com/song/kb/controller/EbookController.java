b129b79a65f19b3eef92720d61f7d298b94d1257
song <songsongsongggg@163.com>
2022-07-03 17:41:24 +0800

    3-2 完成电子书列表查询接口

---
+++ kb/src/main/java/com/song/kb/controller/EbookController.java
@@ -0,0 +1,26 @@
+package com.song.kb.controller;
+
+import com.song.kb.domain.Ebook;
+import com.song.kb.service.EbookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/kb")
+public class EbookController {
+
+    @Autowired
+    private EbookService ebookService;
+    
+
+    @GetMapping("/ebook")
+    public List<Ebook> list(){
+        return ebookService.list();
+    }
+
+}
+
