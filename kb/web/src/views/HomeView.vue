8eb1fba49f3b69d1008e873116eebf39941e248b
song <songsongsongggg@163.com>
2022-07-05 01:28:16 +0800

    5-2 配置axios拦截器打印请求参数和返回参数

--- kb/web/src/views/HomeView.vue
+++ kb/web/src/views/HomeView.vue
@@ -90,17 +90,14 @@ for (let i = 0; i < 23; i++) {
 export default defineComponent({
   name: 'HomeView',
   setup() {
-    console.log("setup");
     const ebooks = ref();
     const ebooks1 = reactive({books: []});
 
     onMounted(() => {
-      console.log("onMounted12312312321");
       axios.get("/ebook/list").then((response) => {
         const data = response.data;
         ebooks.value = data.content;
         ebooks1.books = data.content;
-        console.log(response);
       });
     })
     return {
