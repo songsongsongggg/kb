545303e4a5739a32991e58887f87633a21896577
song <songsongsongggg@163.com>
2022-07-06 23:14:44 +0800

    7-4 分类表设计与持久成代码生成 基本的无限级树表结构设计：id+parent

--- kb/doc/kbdev.sql
+++ kb/doc/kbdev.sql
@@ -1,35 +1,72 @@
 drop table if exists `demo`;
-create table `demo` (
-  `id` int not null comment 'id',
-  `name` varchar(50) comment '名称',
-  primary key (`id`)
-)engine=innodb default charset=utf8mb4 comment='测试';
-
-insert into `demo` (id, name) values (1,'song');
-
-# 点子书表
-drop table if exists `ebook`;
-create table `ebook` (
-    `id` bigint not null comment 'id',
+create table `demo`
+(
+    `id`   int not null comment 'id',
     `name` varchar(50) comment '名称',
+    primary key (`id`)
+) engine = innodb
+  default charset = utf8mb4 comment ='测试';
+
+insert into `demo` (id, name)
+values (1, 'song');
+
+# 电子书表
+drop table if exists `ebook`;
+create table `ebook`
+(
+    `id`           bigint not null comment 'id',
+    `name`         varchar(50) comment '名称',
     `category1_id` bigint comment '分类1',
     `category2_id` bigint comment '分类2',
-    `description` varchar(200) comment '描述',
-    `cover` varchar(200) comment '封面',
-    `doc_count` int comment '文档数',
-    `view_count` int comment '阅读数',
-    `vote_count` int comment '点赞数',
+    `description`  varchar(200) comment '描述',
+    `cover`        varchar(200) comment '封面',
+    `doc_count`    int comment '文档数',
+    `view_count`   int comment '阅读数',
+    `vote_count`   int comment '点赞数',
     primary key (`id`)
-) engine=InnoDB default charset=utf8mb4 comment='电子书';
+) engine = InnoDB
+  default charset = utf8mb4 comment ='电子书';
 
-insert into `ebook` (id,name,description) values (1,'SpringBoot 入门教程','零基础入门 Java 并发，企业级应用开发最佳首选框架');
-insert into `ebook` (id,name,description) values (2,'Vue 入门教程','零基础入门 Vue 并发，企业级应用开发最佳首选框架');
-insert into `ebook` (id,name,description) values (3,'Python 入门教程','零基础入门 Python 并发，企业级应用开发最佳首选框架');
-insert into `ebook` (id,name,description) values (4,'Mysql 入门教程','零基础入门 Mysql 并发，企业级应用开发最佳首选框架');
-insert into `ebook` (id,name,description) values (5,'Oracle 入门教程','零基础入门 Oracle 并发，企业级应用开发最佳首选框架');
+insert into `ebook` (id, name, description)
+values (1, 'SpringBoot 入门教程', '零基础入门 Java 并发，企业级应用开发最佳首选框架');
+insert into `ebook` (id, name, description)
+values (2, 'Vue 入门教程', '零基础入门 Vue 并发，企业级应用开发最佳首选框架');
+insert into `ebook` (id, name, description)
+values (3, 'Python 入门教程', '零基础入门 Python 并发，企业级应用开发最佳首选框架');
+insert into `ebook` (id, name, description)
+values (4, 'Mysql 入门教程', '零基础入门 Mysql 并发，企业级应用开发最佳首选框架');
+insert into `ebook` (id, name, description)
+values (5, 'Oracle 入门教程', '零基础入门 Oracle 并发，企业级应用开发最佳首选框架');
 
 
+# 分类
+drop table if exists `category`;
+create table `category`
+(
+    `id`     bigint       not null comment 'id',
+    `parent` bigint       not null default 0 comment '父id',
+    `name`   varchar(255) not null comment '名称',
+    `sort`   int comment '顺序',
+    primary key (`id`)
+) engine = InnoDB
+  default charset = utf8mb4 comment ='分类';
 
+insert into `category` (id, parent, name, sort) values (100,000, '前端开发',100);
+insert into `category` (id, parent, name, sort) values (101,100, 'Vue',101);
+insert into `category` (id, parent, name, sort) values (102,100, 'HTML & CSS',102);
+insert into `category` (id, parent, name, sort) values (200,000, 'Java',200);
+insert into `category` (id, parent, name, sort) values (201,200, '基础应用',201);
+insert into `category` (id, parent, name, sort) values (202,200, '框架应用',202);
+insert into `category` (id, parent, name, sort) values (300,000, 'Python',300);
+insert into `category` (id, parent, name, sort) values (301,300, '基础应用',301);
+insert into `category` (id, parent, name, sort) values (302,300, '进阶方向应用',302);
+insert into `category` (id, parent, name, sort) values (400,000, '数据库',400);
+insert into `category` (id, parent, name, sort) values (401,400, 'NySQL',401);
+insert into `category` (id, parent, name, sort) values (402,400, 'NoSQL',402);
+insert into `category` (id, parent, name, sort) values (500,000, '其他',500);
+insert into `category` (id, parent, name, sort) values (501,500, '服务器',501);
+insert into `category` (id, parent, name, sort) values (502,500, '开发者工具',502);
+insert into `category` (id, parent, name, sort) values (503,500, '热门服务端语言',503);
 
 
 
