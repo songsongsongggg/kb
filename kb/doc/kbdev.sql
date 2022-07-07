2fddc99690f7f25cae0a7b0f43eb91089cbcf9dc
song <songsongsongggg@163.com>
2022-07-07 16:41:40 +0800

    8-7 增加文档表SQL,并生成持久层代码

--- kb/doc/kbdev.sql
+++ kb/doc/kbdev.sql
@@ -39,7 +39,7 @@ insert into `ebook` (id, name, description)
 values (5, 'Oracle 入门教程', '零基础入门 Oracle 并发，企业级应用开发最佳首选框架');
 
 
-# 分类
+# 分类表
 drop table if exists `category`;
 create table `category`
 (
@@ -51,24 +51,68 @@ create table `category`
 ) engine = InnoDB
   default charset = utf8mb4 comment ='分类';
 
-insert into `category` (id, parent, name, sort) values (100,000, '前端开发',100);
-insert into `category` (id, parent, name, sort) values (101,100, 'Vue',101);
-insert into `category` (id, parent, name, sort) values (102,100, 'HTML & CSS',102);
-insert into `category` (id, parent, name, sort) values (200,000, 'Java',200);
-insert into `category` (id, parent, name, sort) values (201,200, '基础应用',201);
-insert into `category` (id, parent, name, sort) values (202,200, '框架应用',202);
-insert into `category` (id, parent, name, sort) values (300,000, 'Python',300);
-insert into `category` (id, parent, name, sort) values (301,300, '基础应用',301);
-insert into `category` (id, parent, name, sort) values (302,300, '进阶方向应用',302);
-insert into `category` (id, parent, name, sort) values (400,000, '数据库',400);
-insert into `category` (id, parent, name, sort) values (401,400, 'NySQL',401);
-insert into `category` (id, parent, name, sort) values (402,400, 'NoSQL',402);
-insert into `category` (id, parent, name, sort) values (500,000, '其他',500);
-insert into `category` (id, parent, name, sort) values (501,500, '服务器',501);
-insert into `category` (id, parent, name, sort) values (502,500, '开发者工具',502);
-insert into `category` (id, parent, name, sort) values (503,500, '热门服务端语言',503);
+insert into `category` (id, parent, name, sort)
+values (100, 000, '前端开发', 100);
+insert into `category` (id, parent, name, sort)
+values (101, 100, 'Vue', 101);
+insert into `category` (id, parent, name, sort)
+values (102, 100, 'HTML & CSS', 102);
+insert into `category` (id, parent, name, sort)
+values (200, 000, 'Java', 200);
+insert into `category` (id, parent, name, sort)
+values (201, 200, '基础应用', 201);
+insert into `category` (id, parent, name, sort)
+values (202, 200, '框架应用', 202);
+insert into `category` (id, parent, name, sort)
+values (300, 000, 'Python', 300);
+insert into `category` (id, parent, name, sort)
+values (301, 300, '基础应用', 301);
+insert into `category` (id, parent, name, sort)
+values (302, 300, '进阶方向应用', 302);
+insert into `category` (id, parent, name, sort)
+values (400, 000, '数据库', 400);
+insert into `category` (id, parent, name, sort)
+values (401, 400, 'NySQL', 401);
+insert into `category` (id, parent, name, sort)
+values (402, 400, 'NoSQL', 402);
+insert into `category` (id, parent, name, sort)
+values (500, 000, '其他', 500);
+insert into `category` (id, parent, name, sort)
+values (501, 500, '服务器', 501);
+insert into `category` (id, parent, name, sort)
+values (502, 500, '开发者工具', 502);
+insert into `category` (id, parent, name, sort)
+values (503, 500, '热门服务端语言', 503);
 
+# 文档表
+drop table if exists `doc`;
+create table `doc`
+(
+    `id`         bigint       not null comment 'id',
+    `ebook_id`   bigint       not null default 0 comment '电子书id',
+    `parent`     bigint       not null default 0 comment '父id',
+    `name`       varchar(255) not null comment '名称',
+    `sort`       int comment '顺序',
+    `view_count` int                   default 0 comment '阅读数',
+    `cote_count` int                   default 0 comment '点赞数',
+    primary key (`id`)
+) engine = InnoDB
+  default charset = utf8mb4 comment ='文档';
 
+insert into `doc` (id, ebook_id, parent, name, sort, view_count, cote_count)
+values (1, 1, 0, '文档1', 1, 0, 0);
+insert into `doc` (id, ebook_id, parent, name, sort, view_count, cote_count)
+values (2, 1, 1, '文档1.1', 1, 0, 0);
+insert into `doc` (id, ebook_id, parent, name, sort, view_count, cote_count)
+values (3, 1, 0, '文档2', 2, 0, 0);
+insert into `doc` (id, ebook_id, parent, name, sort, view_count, cote_count)
+values (4, 1 ,3, '文档2.1', 1, 0, 0);
+insert into `doc` (id, ebook_id, parent, name, sort, view_count, cote_count)
+values (5, 1, 3, '文档2.2', 2, 0, 0);
+insert into `doc` (id, ebook_id, parent, name, sort, view_count, cote_count)
+values (6, 1, 5, '文档2.2.1', 1, 0, 0);
+insert into `doc` (id, ebook_id, parent, name, sort, view_count, cote_count)
+values (7, 1, 5, '文档2.2.2', 2, 0, 0);
 
 
 
