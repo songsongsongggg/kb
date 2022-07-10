9874385f8de3f3a3b4a180d685fd3b51caab6662
song <songsongsongggg@163.com>
2022-07-10 19:23:55 +0800

    11-11 集成redis,登录成功后将登录信息放入Redis中

--- kb/src/main/java/com/song/kb/controller/UserController.java
+++ kb/src/main/java/com/song/kb/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.song.kb.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.song.kb.req.UserLoginReq;
 import com.song.kb.req.UserQueryReq;
 import com.song.kb.req.UserResetPasswordReq;
@@ -9,19 +10,31 @@ import com.song.kb.resp.PageResp;
 import com.song.kb.resp.UserLoginResp;
 import com.song.kb.resp.UserQueryResp;
 import com.song.kb.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.song.kb.util.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-    @Autowired
+    private final static Logger LOG = LoggerFactory.getLogger(UserController.class);
+
+    @Resource
     private UserService userService;
-    
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    @Resource
+    private SnowFlake snowFlake;
 
     @GetMapping("/list")
     public CommonResp list(@Valid UserQueryReq req){
@@ -61,6 +74,12 @@ public class UserController {
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp<UserLoginResp> resp = new CommonResp<>();
         UserLoginResp userLoginResp = userService.login(req);
+
+        Long token = snowFlake.nextId();
+        LOG.info("生成单点登陆token:{},并放入redis中",token);
+        userLoginResp.setToken(token);
+        redisTemplate.opsForValue().set(token, JSONObject.toJSONString(userLoginResp),3600 * 24, TimeUnit.SECONDS);
+
         resp.setContent(userLoginResp);
         return resp;
     }
--- kb/src/main/java/com/song/kb/resp/UserLoginResp.java
+++ kb/src/main/java/com/song/kb/resp/UserLoginResp.java
@@ -8,6 +8,16 @@ public class UserLoginResp {
 
     private String name;
 
+    private Long token;
+
+    public Long getToken() {
+        return token;
+    }
+
+    public void setToken(Long token) {
+        this.token = token;
+    }
+
     public Long getId() {
         return id;
     }
@@ -32,16 +42,15 @@ public class UserLoginResp {
         this.name = name;
     }
 
+
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", loginName=").append(loginName);
-        sb.append(", name=").append(name);
-        sb.append("]");
-        return sb.toString();
+        return "UserLoginResp{" +
+                "id=" + id +
+                ", loginName='" + loginName + '\'' +
+                ", name='" + name + '\'' +
+                ", token=" + token +
+                '}';
+
     }
 }
