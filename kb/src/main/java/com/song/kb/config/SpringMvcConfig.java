87fee0b194d000a1a1731fb55e4a9a71fa6f1da7
song <songsongsongggg@163.com>
2022-07-11 00:13:33 +0800

    11-17 前后端增加拦截 修复BUG：前端储存token，统一用String类型

--- kb/src/main/java/com/song/kb/config/SpringMvcConfig.java
+++ kb/src/main/java/com/song/kb/config/SpringMvcConfig.java
@@ -1,33 +1,33 @@
-//package com.song.kb.config;
-//
-//import com.song.kb.interceptor.LoginInterceptor;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//import javax.annotation.Resource;
-//
-//@Configuration
-//public class SpringMvcConfig implements WebMvcConfigurer {
-//
-//    @Resource
-//    LoginInterceptor loginInterceptor;
-//
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(loginInterceptor)
-//                .addPathPatterns("/**")
-//                .excludePathPatterns(
-//                        "/test/**",
-//                        "/redis/**",
-//                        "/user/login",
-//                        "/category/all",
-//                        "/ebook/list",
-//                        "/doc/all/**",
-//                        "/doc/vote/**",
-//                        "/doc/find-content/**",
-//                        "/ebook-snapshot/**",
-//                        "login"
-//                );
-//
-//    }
-//}
+package com.song.kb.config;
+
+import com.song.kb.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LoginInterceptor loginInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/test/**",
+                        "/redis/**",
+                        "/user/login",
+                        "/category/all",
+                        "/ebook/list",
+                        "/doc/all/**",
+                        "/doc/vote/**",
+                        "/doc/find-content/**",
+                        "/ebook-snapshot/**",
+                        "login"
+                );
+
+    }
+}
