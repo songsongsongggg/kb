6d8e55be83f3aa22e62d32e0565b730784f07a5f
song <songsongsongggg@163.com>
2022-07-10 19:49:34 +0800

    11-13 前端显示登录昵称，遗留问题，刷新会没有

--- kb/web/src/components/the-header.vue
+++ kb/web/src/components/the-header.vue
@@ -22,7 +22,10 @@
       <a-menu-item key="/about">
         <router-link to="/about">关于我们</router-link>
       </a-menu-item>
-      <a class="login-menu" @click="showLoginModal()">
+      <a class="login-menu" v-show="user.id">
+        <span>{{ user.name }}</span>
+      </a>
+      <a class="login-menu" v-show="!user.id" @click="showLoginModal()">
         <span>登录</span>
       </a>
     </a-menu>
@@ -56,9 +59,12 @@ export default defineComponent({
   name: 'the-header',
   setup() {
 
+    const user = ref();
+    user.value = "";
+
     const loginUser = ref({
       loginName: "test",
-      password: "test",
+      password: "test1234",
     });
 
     const loginModalVisible = ref(false);
@@ -78,6 +84,7 @@ export default defineComponent({
         if (data.success) {
           loginModalVisible.value = false;
           message.success("登录成功！");
+          user.value = data.content;
         } else {
           message.error(data.message);
         }
@@ -89,6 +96,7 @@ export default defineComponent({
       showLoginModal,
       loginUser,
       login,
+      user,
 
     };
 
